abdb5d76debedf6652af743c73af4415447e4a30
Akifumi1119 <akifumi.doi1119@gmail.com>
2026-09-08 14:46:44 +0900

    20260908 デプロイ準備3(CORS設定の修正)

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://Akifumi1119.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
